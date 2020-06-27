226c518b4fce19d6bad6e2995bde7dc62f96136d
egorcrytski <egorcrytski@gmail.com>
2020-06-27 05:51:36 +0300

    filtering fixes

--- projement/projects/admin.py
+++ projement/projects/admin.py
@@ -44,7 +44,7 @@ class CompaniesListFilter(admin.SimpleListFilter):
             return queryset.filter(company_id=self.value())
         return queryset
 
-'''class CompaniesTagsListFilter(admin.SimpleListFilter):
+class CompaniesTagsListFilter(admin.SimpleListFilter):
     """
     This filter will always return a subset of the instances in a Model, either filtering by the
     user choice or by a default value.
@@ -67,10 +67,10 @@ class CompaniesListFilter(admin.SimpleListFilter):
         in the right sidebar.
         """
         list_of_tags = []
-        queryset = Tag.objects.all()
+        queryset = ProjectTag.objects.all()
         for tag in queryset:
             list_of_tags.append(
-                (str(tag.id), tag.tagname)
+                (str(tag.project.id), tag.tag.tagname)
             )
         return sorted(list_of_tags, key=lambda tp: tp[1])
 
@@ -83,13 +83,13 @@ class CompaniesListFilter(admin.SimpleListFilter):
         # Compare the requested value to decide how to filter the queryset.
         if self.value():
             return queryset.filter(id=self.value())
-        return queryset'''
+        return queryset
 
 
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'company', 'start_date', 'end_date')
-    list_filter = ('company__name', 'company__id', CompaniesListFilter)#, CompaniesTagsListFilter)#CompaniesTagsListFilter) #, 'show_tags')
+    list_filter = ('company__name', 'company__id', CompaniesListFilter, CompaniesTagsListFilter)
     ordering = ('-start_date',)
 
     fieldsets = (
